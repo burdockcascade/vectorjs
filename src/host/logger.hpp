aa31322779f2b40ae66e2aa4f731159619921429
Peter Richmond <3448039+burdockcascade@users.noreply.github.com>
2026-04-12 10:26:46 +0100

    refactor: simplify logger

--- src/host/logger.hpp
+++ src/host/logger.hpp
@@ -1,10 +1,26 @@
 #pragma once
+
 #include <spdlog/spdlog.h>
 #include <utility>
+#include <raylib.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 
 namespace Log {
-    void Init();
-    void SetLevel(spdlog::level::level_enum level);
+
+    inline void Init() {
+        const auto logger = spdlog::stdout_color_mt("APP");
+        spdlog::set_default_logger(logger);
+
+        spdlog::set_pattern("%^[%T.%e] [%l] %v%$");
+        spdlog::set_level(spdlog::level::info); // Set initial level
+        spdlog::flush_on(spdlog::level::err);
+
+        SetTraceLogLevel(LOG_NONE);
+    }
+
+    inline void SetLevel(const spdlog::level::level_enum level) {
+        spdlog::set_level(level);
+    }
 
     // Variadic templates to replace macros
     template<typename... Args>
