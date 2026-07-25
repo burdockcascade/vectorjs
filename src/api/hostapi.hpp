3c41e3edf6606a118ec968db9f69245d81ce7a52
Peter Richmond <3448039+burdockcascade@users.noreply.github.com>
2026-07-25 20:54:21 +0100

    fix: change RAYLIB_VERSION_STR and QUICKJS_VERSION_STR to inline variables in hostapi.hpp

--- src/api/hostapi.hpp
+++ src/api/hostapi.hpp
@@ -4,8 +4,8 @@
 #include <quickjs.h>
 
 namespace HostApi {
-    const std::string RAYLIB_VERSION_STR = std::format("{}.{}.{}", RAYLIB_VERSION_MAJOR, RAYLIB_VERSION_MINOR, RAYLIB_VERSION_PATCH);
-    const std::string QUICKJS_VERSION_STR = std::format("{}.{}.{}", QJS_VERSION_MAJOR, QJS_VERSION_MINOR, QJS_VERSION_PATCH);
+    inline const std::string RAYLIB_VERSION_STR = std::format("{}.{}.{}", RAYLIB_VERSION_MAJOR, RAYLIB_VERSION_MINOR, RAYLIB_VERSION_PATCH);
+    inline const std::string QUICKJS_VERSION_STR = std::format("{}.{}.{}", QJS_VERSION_MAJOR, QJS_VERSION_MINOR, QJS_VERSION_PATCH);
 
     void register_hapi_enums(JSContext* ctx, JSModuleDef* m);
     void register_hapi_classes(JSContext* ctx, JSModuleDef* m);
