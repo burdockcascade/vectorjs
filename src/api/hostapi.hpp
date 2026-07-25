2401d311d39972b336ca0d426a65d5bf70b8d175
Peter Richmond <3448039+burdockcascade@users.noreply.github.com>
2026-07-25 22:22:35 +0100

    fix: remove Font export from module in hostapi.hpp

--- src/api/hostapi.hpp
+++ src/api/hostapi.hpp
@@ -22,7 +22,6 @@ namespace HostApi {
             JS_AddModuleExport(ctx, m, "Color");
             JS_AddModuleExport(ctx, m, "Vector2");
             JS_AddModuleExport(ctx, m, "Rectangle");
-            JS_AddModuleExport(ctx, m, "Font");
             JS_AddModuleExport(ctx, m, "Palette");
             JS_AddModuleExport(ctx, m, "Info");
             JS_AddModuleExport(ctx, m, "ConfigFlags");
