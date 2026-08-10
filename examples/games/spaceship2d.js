01efbfdf8cad52a14e263cef93b576bf5ff4d5a0
Peter Richmond <3448039+burdockcascade@users.noreply.github.com>
2026-08-10 17:24:38 +0100

    example: initialize spaceship2d application with basic setup

---
+++ examples/games/spaceship2d.js
@@ -0,0 +1,12 @@
+import {Application, Rectangle, Vector2, Palette } from "vectorjs";
+
+const screenWidth = 600;
+const screenHeight = 800;
+
+const app = new Application(screenWidth, screenHeight,"Window");
+app.run({
+
+    onDraw(render) {
+
+    }
+});
