199fe82eb5b3dcd9c9f2dfc52acfc9fff5a6aff1
Peter Richmond <3448039+burdockcascade@users.noreply.github.com>
2026-08-19 20:19:08 +0100

    refactor: capture engine reference in audio instance methods for consistency

--- src/modules/vectorjs/vjs_audio.cpp
+++ src/modules/vectorjs/vjs_audio.cpp
@@ -16,43 +16,43 @@ namespace App::Modules {
         });
 
         // Instance Method: mySound.play()
-        sound.instance_method("play", [](JSSound* self, const qjspp::ArgList& args) -> qjspp::Value {
+        sound.instance_method("play", [&engine](JSSound* self, const qjspp::ArgList& args) -> qjspp::Value {
             if (self && self->sound_ptr) {
                 PlaySound(*(self->sound_ptr));
             }
-            return {};
+            return engine.make_undefined();
         });
 
         // Instance Method: mySound.pause()
-        sound.instance_method("pause", [](JSSound* self, const qjspp::ArgList& args) -> qjspp::Value {
+        sound.instance_method("pause", [&engine](JSSound* self, const qjspp::ArgList& args) -> qjspp::Value {
             if (self && self->sound_ptr) {
                 PauseSound(*(self->sound_ptr));
             }
-            return {};
+            return engine.make_undefined();
         });
 
         // Instance Method: mySound.resume()
-        sound.instance_method("resume", [](JSSound* self, const qjspp::ArgList& args) -> qjspp::Value {
+        sound.instance_method("resume", [&engine](JSSound* self, const qjspp::ArgList& args) -> qjspp::Value {
             if (self && self->sound_ptr) {
                 ResumeSound(*(self->sound_ptr));
             }
-            return {};
+            return engine.make_undefined();
         });
 
         // Instance Method: mySound.stop()
-        sound.instance_method("stop", [](JSSound* self, const qjspp::ArgList& args) -> qjspp::Value {
+        sound.instance_method("stop", [&engine](JSSound* self, const qjspp::ArgList& args) -> qjspp::Value {
             if (self && self->sound_ptr) {
                 StopSound(*(self->sound_ptr));
             }
-            return {};
+            return engine.make_undefined();
         });
 
         // Instance Method: mySound.setVolume(0.5)
-        sound.instance_method("setVolume", [](JSSound* self, const qjspp::ArgList& args) -> qjspp::Value {
+        sound.instance_method("setVolume", [&engine](JSSound* self, const qjspp::ArgList& args) -> qjspp::Value {
             if (self && self->sound_ptr && !args.empty()) {
                 SetSoundVolume(*(self->sound_ptr), static_cast<float>(args[0].to_double()));
             }
-            return {};
+            return engine.make_undefined();
         });
 
         builder.export_class("Sound", sound.build());
@@ -68,51 +68,51 @@ namespace App::Modules {
         });
 
         // Instance Method: myMusic.play()
-        music.instance_method("play", [](JSMusic* self, const qjspp::ArgList& args) -> qjspp::Value {
+        music.instance_method("play", [&engine](JSMusic* self, const qjspp::ArgList& args) -> qjspp::Value {
             if (self && self->music_ptr) {
                 PlayMusicStream(*(self->music_ptr));
             }
-            return {};
+            return engine.make_undefined();
         });
 
         // Instance Method: myMusic.pause()
-        music.instance_method("pause", [](JSMusic* self, const qjspp::ArgList& args) -> qjspp::Value {
+        music.instance_method("pause", [&engine](JSMusic* self, const qjspp::ArgList& args) -> qjspp::Value {
             if (self && self->music_ptr) {
                 PauseMusicStream(*(self->music_ptr));
             }
-            return {};
+            return engine.make_undefined();
         });
 
         // Instance Method: myMusic.resume()
-        music.instance_method("resume", [](JSMusic* self, const qjspp::ArgList& args) -> qjspp::Value {
+        music.instance_method("resume", [&engine](JSMusic* self, const qjspp::ArgList& args) -> qjspp::Value {
             if (self && self->music_ptr) {
                 ResumeMusicStream(*(self->music_ptr));
             }
-            return {};
+            return engine.make_undefined();
         });
 
         // Instance Method: myMusic.stop()
-        music.instance_method("stop", [](JSMusic* self, const qjspp::ArgList& args) -> qjspp::Value {
+        music.instance_method("stop", [&engine](JSMusic* self, const qjspp::ArgList& args) -> qjspp::Value {
             if (self && self->music_ptr) {
                 StopMusicStream(*(self->music_ptr));
             }
-            return {};
+            return engine.make_undefined();
         });
 
         // Instance Method: myMusic.update()
-        music.instance_method("update", [](JSMusic* self, const qjspp::ArgList& args) -> qjspp::Value {
+        music.instance_method("update", [&engine](JSMusic* self, const qjspp::ArgList& args) -> qjspp::Value {
             if (self && self->music_ptr) {
                 UpdateMusicStream(*(self->music_ptr));
             }
-            return {};
+            return engine.make_undefined();
         });
 
         // Instance Method: myMusic.setVolume(0.5)
-        music.instance_method("setVolume", [](JSMusic* self, const qjspp::ArgList& args) -> qjspp::Value {
+        music.instance_method("setVolume", [&engine](JSMusic* self, const qjspp::ArgList& args) -> qjspp::Value {
             if (self && self->music_ptr && !args.empty()) {
                 SetMusicVolume(*(self->music_ptr), static_cast<float>(args[0].to_double()));
             }
-            return {};
+            return engine.make_undefined();
         });
 
         builder.export_class("Music", music.build());
