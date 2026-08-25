9dccfb80a3082e4f462020413a8732f5fbdf63b1
Peter Richmond <3448039+burdockcascade@users.noreply.github.com>
2026-08-25 15:31:42 +0100

    refactor: cleanup engien header

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -1,15 +1,143 @@
 #include "engine.hpp"
+#include <ranges>
+#include <variant>
+#include "raylib.h"
 
 namespace Hooray {
+
     namespace {
         template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
         template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
     }
 
+    // ==========================================
+    // LifecycleManager Implementation
+    // ==========================================
+
+    void LifecycleManager::add_listener(const std::shared_ptr<LifecycleListener>& listener) {
+        listeners_.push_back(listener);
+    }
+
+    void LifecycleManager::remove_listener(const std::shared_ptr<LifecycleListener>& listener) {
+        std::erase(listeners_, listener);
+    }
+
+    void LifecycleManager::notify_init() {
+        for (auto& listener : listeners_) {
+            listener->on_init();
+        }
+    }
+
+    void LifecycleManager::notify_update(const float delta_time) {
+        for (auto& listener : listeners_) {
+            listener->on_update(delta_time);
+        }
+    }
+
+    void LifecycleManager::notify_draw() {
+        for (auto& listener : listeners_) {
+            listener->on_draw();
+        }
+    }
+
+    // ==========================================
+    // InputManager Implementation
+    // ==========================================
+
+    void InputManager::add_listener(const std::shared_ptr<InputListener>& listener) {
+        listeners_.push_back(listener);
+    }
+
+    void InputManager::remove_listener(const std::shared_ptr<InputListener>& listener) {
+        std::erase(listeners_, listener);
+    }
+
+    bool InputManager::is_key_down(const int key) {
+        return IsKeyDown(key);
+    }
+
+    bool InputManager::is_mouse_button_down(const int button) {
+        return IsMouseButtonDown(button);
+    }
+
+    Vector2 InputManager::get_mouse_position() {
+        return GetMousePosition();
+    }
+
+    Vector2 InputManager::get_mouse_delta() {
+        return GetMouseDelta();
+    }
+
+    void InputManager::process_frame() {
+        // 1. Keyboard Events
+        int key = GetKeyPressed();
+        while (key > 0) {
+            dispatch_key_pressed(key);
+            key = GetKeyPressed();
+        }
+
+        // 2. Mouse Press & Release Events
+        const Vector2 mouse_pos = GetMousePosition();
+        for (int button = MOUSE_BUTTON_LEFT; button <= MOUSE_BUTTON_BACK; ++button) {
+            if (IsMouseButtonPressed(button)) {
+                dispatch_mouse_pressed(button, mouse_pos);
+            }
+            if (IsMouseButtonReleased(button)) {
+                dispatch_mouse_released(button, mouse_pos);
+            }
+        }
+
+        // 3. Mouse Movement Events
+        const Vector2 delta = GetMouseDelta();
+        if (delta.x != 0.0f || delta.y != 0.0f) {
+            dispatch_mouse_moved(mouse_pos);
+        }
+    }
+
+    void InputManager::dispatch_key_pressed(const int key) {
+        for (auto& listener : std::views::reverse(listeners_)) {
+            if (listener->on_key_pressed(key)) break;
+        }
+    }
+
+    void InputManager::dispatch_mouse_pressed(const int button, const Vector2 pos) {
+        for (auto& listener : std::views::reverse(listeners_)) {
+            if (listener->on_mouse_pressed(button, pos)) break;
+        }
+    }
+
+    void InputManager::dispatch_mouse_released(const int button, const Vector2 pos) {
+        for (auto& listener : std::views::reverse(listeners_)) {
+            if (listener->on_mouse_released(button, pos)) break;
+        }
+    }
+
+    void InputManager::dispatch_mouse_moved(const Vector2 pos) {
+        for (auto& listener : std::views::reverse(listeners_)) {
+            if (listener->on_mouse_moved(pos)) break;
+        }
+    }
+
+    // ==========================================
+    // Engine Implementation
+    // ==========================================
+
     Engine::Engine(const int width, const int height, std::string title)
         : width_(width), height_(height), title_(std::move(title)) {}
 
     Engine::~Engine() = default;
+
+    void Engine::set_on_init(Callback callback) {
+        on_init_ = std::move(callback);
+    }
+
+    void Engine::set_on_update(UpdateCallback callback) {
+        on_update_ = std::move(callback);
+    }
+
+    void Engine::set_on_draw(Callback callback) {
+        on_draw_ = std::move(callback);
+    }
 
     void Engine::run() {
         run_loop(true);
--- src/engine/engine.hpp
+++ src/engine/engine.hpp
@@ -2,10 +2,9 @@
 
 #include <functional>
 #include <memory>
-#include <ranges>
 #include <string>
+#include <vector>
 #include "commands.hpp"
-#include "raylib.h"
 
 namespace Hooray {
 
@@ -19,31 +18,12 @@ namespace Hooray {
 
     class LifecycleManager {
     public:
-        void add_listener(const std::shared_ptr<LifecycleListener>& listener) {
-            listeners_.push_back(listener);
-        }
+        void add_listener(const std::shared_ptr<LifecycleListener>& listener);
+        void remove_listener(const std::shared_ptr<LifecycleListener>& listener);
 
-        void remove_listener(const std::shared_ptr<LifecycleListener>& listener) {
-            std::erase(listeners_, listener);
-        }
-
-        void notify_init() {
-            for (auto& listener : listeners_) {
-                listener->on_init();
-            }
-        }
-
-        void notify_update(float delta_time) {
-            for (auto& listener : listeners_) {
-                listener->on_update(delta_time);
-            }
-        }
-
-        void notify_draw() {
-            for (auto& listener : listeners_) {
-                listener->on_draw();
-            }
-        }
+        void notify_init();
+        void notify_update(float delta_time);
+        void notify_draw();
 
     private:
         std::vector<std::shared_ptr<LifecycleListener>> listeners_;
@@ -60,78 +40,29 @@ namespace Hooray {
 
     class InputManager {
     public:
-        void add_listener(const std::shared_ptr<InputListener>& listener) {
-            listeners_.push_back(listener);
-        }
-
-        void remove_listener(const std::shared_ptr<InputListener>& listener) {
-            std::erase(listeners_, listener);
-        }
+        void add_listener(const std::shared_ptr<InputListener>& listener);
+        void remove_listener(const std::shared_ptr<InputListener>& listener);
 
         // --- State Polling API ---
-        static bool is_key_down(int key) { return IsKeyDown(key); }
-        static bool is_mouse_button_down(int button) { return IsMouseButtonDown(button); }
-        static Vector2 get_mouse_position() { return GetMousePosition(); }
-        static Vector2 get_mouse_delta() { return GetMouseDelta(); }
+        static bool is_key_down(int key);
+        static bool is_mouse_button_down(int button);
+        static Vector2 get_mouse_position();
+        static Vector2 get_mouse_delta();
 
         // --- Process Discrete Events (Called once per frame) ---
-        void process_frame() {
-            // 1. Keyboard Events
-            int key = GetKeyPressed();
-            while (key > 0) {
-                dispatch_key_pressed(key);
-                key = GetKeyPressed();
-            }
-
-            // 2. Mouse Press & Release Events
-            const Vector2 mouse_pos = GetMousePosition();
-            for (int button = MOUSE_BUTTON_LEFT; button <= MOUSE_BUTTON_BACK; ++button) {
-                if (IsMouseButtonPressed(button)) {
-                    dispatch_mouse_pressed(button, mouse_pos);
-                }
-                if (IsMouseButtonReleased(button)) {
-                    dispatch_mouse_released(button, mouse_pos);
-                }
-            }
-
-            // 3. Mouse Movement Events
-            const Vector2 delta = GetMouseDelta();
-            if (delta.x != 0.0f || delta.y != 0.0f) {
-                dispatch_mouse_moved(mouse_pos);
-            }
-        }
+        void process_frame();
 
     private:
         std::vector<std::shared_ptr<InputListener>> listeners_;
 
-        void dispatch_key_pressed(int key) {
-            for (auto& listener : std::views::reverse(listeners_)) {
-                if (listener->on_key_pressed(key)) break;
-            }
-        }
-
-        void dispatch_mouse_pressed(int button, Vector2 pos) {
-            for (auto& listener : std::views::reverse(listeners_)) {
-                if (listener->on_mouse_pressed(button, pos)) break;
-            }
-        }
-
-        void dispatch_mouse_released(int button, Vector2 pos) {
-            for (auto& listener : std::views::reverse(listeners_)) {
-                if (listener->on_mouse_released(button, pos)) break;
-            }
-        }
-
-        void dispatch_mouse_moved(Vector2 pos) {
-            for (auto& listener : std::views::reverse(listeners_)) {
-                if (listener->on_mouse_moved(pos)) break;
-            }
-        }
+        void dispatch_key_pressed(int key);
+        void dispatch_mouse_pressed(int button, Vector2 pos);
+        void dispatch_mouse_released(int button, Vector2 pos);
+        void dispatch_mouse_moved(Vector2 pos);
     };
 
     class Engine {
     public:
-
         using Callback = std::function<void()>;
         using UpdateCallback = std::function<void(float)>;
         using KeyCallback = std::function<void(int key)>;
@@ -145,12 +76,12 @@ namespace Hooray {
         Engine(const Engine&) = delete;
         Engine& operator=(const Engine&) = delete;
 
-        // Existing callback setters
-        void set_on_init(Callback callback) { on_init_ = std::move(callback); }
-        void set_on_update(UpdateCallback callback) { on_update_ = std::move(callback); }
-        void set_on_draw(Callback callback) { on_draw_ = std::move(callback); }
+        // Callback setters
+        void set_on_init(Callback callback);
+        void set_on_update(UpdateCallback callback);
+        void set_on_draw(Callback callback);
 
-        // Access to the Input System
+        // Access to Managers
         InputManager& get_input_manager() { return input_manager_; }
         [[nodiscard]] const InputManager& get_input_manager() const { return input_manager_; }
 
@@ -158,9 +89,7 @@ namespace Hooray {
         [[nodiscard]] const LifecycleManager& get_lifecycle_manager() const { return lifecycle_manager_; }
 
         void run();
-
         void display();
-
         void run_loop(bool clear_buffer_after_frame);
 
         CommandBufferBuilder& get_buffer() { return command_buffer_; }
@@ -178,7 +107,6 @@ namespace Hooray {
         UpdateCallback on_update_;
         Callback on_draw_;
 
-        // New callback variables
         KeyCallback on_key_pressed_;
         MouseButtonCallback on_mouse_pressed_;
         MouseMoveCallback on_mouse_move_;
